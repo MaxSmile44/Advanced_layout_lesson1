6018de411d5ae2033ce7be1b03655044ef4b4725
MaxSmile44 <afmaxim89@gmail.com>
2025-02-24 10:20:38 +0300

    Add static index.html in main folder

--- Advanced_layout_lesson1/settings.py
+++ Advanced_layout_lesson1/settings.py
@@ -132,7 +132,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.getenv('STATIC_ROOT')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
